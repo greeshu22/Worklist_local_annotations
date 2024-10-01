ea8a9e3817f5c7b906ac44541a7c6f4c423484cf
greeshma KUDERU <greeshmakuderu2@gmail.com>
2024-10-01 22:36:48 +0000

    firts commit

---
+++ Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.anjali.worklistpagelocalannotations.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
